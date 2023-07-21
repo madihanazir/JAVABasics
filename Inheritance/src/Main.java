e4e83ac119d7922bc616fe68c4ef8f7df23b9807
madihanazir <125150474+madihanazir@users.noreply.github.com>
2023-07-21 23:37:53 +0530

    Update Main.java

--- Inheritance/src/Main.java
+++ Inheritance/src/Main.java
@@ -5,7 +5,8 @@ public class Main {
 			 Mouse1 m1= new Mouse1();
 			 m1.leftClick();
 			 m1.rightClick();
-			 m1.scrollDown();m1.scrollUp();
+			 m1.scrollDown();
+			 m1.scrollUp();
 			 
 			
 			 Mouse2 m2= new Mouse2();
