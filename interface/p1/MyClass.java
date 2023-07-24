da39ab8f4b35d0a1df2f8f1fbccb3ce37b218d96
madihanazir <125150474+madihanazir@users.noreply.github.com>
2023-07-24 22:06:37 +0530

    Add files via upload

---
+++ interface/p1/MyClass.java
@@ -0,0 +1,16 @@
+package p1;
+
+public class MyClass implements A {
+	public void meth1()
+	{
+		System.out.println("This is method1");
+	}
+public void meth2() {
+	System.out.println("this is method 2");
+	
+}
+
+	
+
+
+}
